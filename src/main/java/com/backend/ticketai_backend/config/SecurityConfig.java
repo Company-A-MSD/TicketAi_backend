8d57fc799521255f81a81841a896947ef011c0e1
RavimalRanathunga <ravimalranathunga@gmail.com>
2025-05-14 22:56:54 +0530

    remove root protection

--- src/main/java/com/backend/ticketai_backend/config/SecurityConfig.java
+++ src/main/java/com/backend/ticketai_backend/config/SecurityConfig.java
@@ -23,7 +23,7 @@ public class SecurityConfig {
         .csrf(csrf -> csrf.disable())
         .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .authorizeHttpRequests(auth -> auth
-        .requestMatchers("/api/employees/login/**").permitAll()
+        .requestMatchers("/api/employees/login/**","/").permitAll()
         .requestMatchers("/api/employees/**").hasRole("ADMIN")
         .anyRequest().authenticated()
     )
