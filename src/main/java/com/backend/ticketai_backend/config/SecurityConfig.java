4db8e55367cbb10a2f974824771f21b56fded3bd
RavimalRanathunga <ravimalranathunga@gmail.com>
2025-05-17 23:15:10 +0530

    create ticket management module and add some security configurations

--- src/main/java/com/backend/ticketai_backend/config/SecurityConfig.java
+++ src/main/java/com/backend/ticketai_backend/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.backend.ticketai_backend.security.JwtAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -23,8 +24,12 @@ public class SecurityConfig {
         .csrf(csrf -> csrf.disable())
         .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .authorizeHttpRequests(auth -> auth
-        .requestMatchers("/api/employees/login/**","/").permitAll()
-        .requestMatchers("/api/employees/**").hasRole("ADMIN")
+        .requestMatchers("/api/employees/login/**","/","/api/register","/api/login").permitAll()
+        .requestMatchers("/api/employees/**","/api/tickets/all","/api/users/**","/api/delete_user").hasRole("ADMIN")
+        .requestMatchers("/api/tickets/assigned","/api/notifications/on-ticket-update").hasRole("EMPLOYEE")
+        .requestMatchers("/api/tickets","/api/delete_account","/api/notifications").hasRole("USER")
+        .requestMatchers(HttpMethod.GET,"/api/tickets/:ticketId").hasAnyRole("USER","EMPLOYEE","ADMIN")
+        .requestMatchers(HttpMethod.PATCH,"/api/tickets/:ticketId").hasAnyRole("EMPLOYEE","ADMIN")
         .anyRequest().authenticated()
     )
     .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
---
+++ src/main/java/com/backend/ticketai_backend/ticket_management/dto/CreateTicketRequestDto.java
@@ -0,0 +1,34 @@
+package com.backend.ticketai_backend.ticket_management.dto;
+
+public class CreateTicketRequestDto {
+
+    private String subject;
+    private String description;
+    private String channel;
+
+    // Getters and Setters
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getChannel() {
+        return channel;
+    }
+
+    public void setChannel(String channel) {
+        this.channel = channel;
+    }
+
+}
