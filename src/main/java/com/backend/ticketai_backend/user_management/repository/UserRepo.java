524be6cc93a3f60d1f25e736c156553947b6a395
ChithminiJayangi <chithminijayangi@gmail.com>
2025-07-02 18:23:54 +0530

    add functions for user login and register

--- src/main/java/com/backend/ticketai_backend/user_management/repository/UserRepo.java
+++ src/main/java/com/backend/ticketai_backend/user_management/repository/UserRepo.java
@@ -4,6 +4,7 @@ import com.backend.ticketai_backend.user_management.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface UserRepo extends MongoRepository<User,String> {
+    User findByEmail(String email);
     
 
 }
